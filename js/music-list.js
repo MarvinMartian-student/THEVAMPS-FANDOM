f0b85a66213491dcd5fb23c4bc1b47c769fe0801
marvin_landicho <marvin.landicho@cvsu.edu.ph>
2024-07-01 17:00:02 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Oh Cecilia Breaking My Heart ft.shawn Mendes",
+    artist: "The Vamps",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Wild Heart",
+    artist: "The Vamps",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Last Night",
+    artist: "The Vamps",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Can We Dance",
+    artist: "The Vamps",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Just My Type",
+    artist: "The Vamps",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "I Found The Girl ft. Omi ",
+    artist: "The Vamps",
+    img: "music-6",
+    src: "music-6"
+  },
+   
+];
